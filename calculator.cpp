cf141fc91e0934ca31a4b8593ff59820ef8f8770
kevin <kchicas314@gmail.com>
2023-05-26 16:05:23 -0400

    addition added

--- calculator.cpp
+++ calculator.cpp
@@ -1,2 +1,33 @@
+#include <iostream>
+#include <unistd.h>
+#include <cmath>
 using namespace std;
-djfnjfn
+//calculator program
+
+int addNums(int a, int b){
+    cout<<a+b<<endl;
+    return a+b;
+}
+
+int main(){
+    int userInput1 = 0;
+    int userInput2 =0;
+    int userOp=0;
+    int result =0;
+    cout<<"Enter num #1"<<endl;
+    cin>>userInput1;
+    cout<<"Enter num #2"<<endl;
+    cin>>userInput2;
+    cout<<"Now choose what type of operation"<<endl;
+    cin>>userOp;
+    switch (userOp)
+    {
+    case 1:
+    cout<<"Additon has been chosen!"<<endl;
+        result = addNums(userInput1,userInput2);
+        cout<<result<<endl;
+        break;
+    default:
+        break;
+    }
+}
